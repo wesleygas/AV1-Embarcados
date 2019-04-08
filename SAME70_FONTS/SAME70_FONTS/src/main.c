370f63cf4cec0b50f6f5a5c04c6ca065ae188dd8
wesleygas <wesleygl16@gmail.com>
2019-04-08 15:44:00 -0300

    RTT+PIOISR

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -14,6 +14,26 @@
 
 struct ili9488_opt_t g_ili9488_display_opt;
 
+//IOS
+//OLEDBoard
+#define BUT1_PIO		  PIOD
+#define BUT1_MASK		  (1u << 28u)
+
+#define BUT2_PIO		  PIOC
+#define BUT2_MASK		  (1u << 31)
+
+#define BUT3_PIO		  PIOA
+#define BUT3_MASK		  (1u << 19)
+
+//Onboard LED
+#define  LED_PIO  PIOC
+#define  LED_MASK (1u << 8u)
+
+//Var Globais
+volatile int roda_voltas = 0;
+volatile int update_alarm = 1;
+
+
 void configure_lcd(void){
 	/* Initialize display parameter */
 	g_ili9488_display_opt.ul_width = ILI9488_LCD_WIDTH;
@@ -26,6 +46,114 @@ void configure_lcd(void){
 	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
 	
 }
+
+void RTT_Handler(void)
+{
+	uint32_t ul_status;
+
+	/* Get RTT status */
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {
+		
+	  }
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		update_alarm = true;                  // flag RTT alarme
+	}
+}
+
+void rtt_reconfigure(){
+	 
+      /*
+       * O clock base do RTT é 32678Hz
+       * Para gerar outra base de tempo é necessário
+       * usar o PLL pre scale, que divide o clock base.
+       *
+       * Nesse exemplo, estamos operando com um clock base
+       * de pllPreScale = 32768/32768/2 = 2Hz
+       *
+       * Quanto maior a frequência maior a resolução, porém
+       * menor o tempo máximo que conseguimos contar.
+       *
+       * Podemos configurar uma IRQ para acontecer quando 
+       * o contador do RTT atingir um determinado valor
+       * aqui usamos o irqRTTvalue para isso.
+       * 
+       * Nesse exemplo o irqRTTvalue = 8, causando uma
+       * interrupção a cada 2 segundos (lembre que usamos o 
+       * pllPreScale, cada incremento do RTT leva 500ms (2Hz).
+       */
+      uint16_t pllPreScale = (int) (((float) 32768) / 1.0);
+      uint32_t irqRTTvalue  = 4;
+      
+      // reinicia RTT para gerar um novo IRQ
+      RTT_init(pllPreScale, irqRTTvalue);         
+      
+     /*
+      * caso queira ler o valor atual do RTT, basta usar a funcao
+      *   rtt_read_timer_value()
+      */
+      
+      /*
+       * CLEAR FLAG
+       */
+}
+
+void magsense_callback(void){
+	roda_voltas++;
+}
+
+void io_init(void){
+	pmc_enable_periph_clk(ID_PIOA);
+	//pmc_enable_periph_clk(ID_PIOB);
+	pmc_enable_periph_clk(ID_PIOC);
+	pmc_enable_periph_clk(ID_PIOD);
+	
+	pio_configure(BUT3_PIO, PIO_INPUT,BUT3_MASK,PIO_DEBOUNCE|PIO_PULLUP);
+	pio_configure(BUT2_PIO, PIO_INPUT,BUT2_MASK,PIO_DEBOUNCE|PIO_PULLUP);
+	pio_configure(BUT1_PIO, PIO_INPUT,BUT1_MASK,PIO_DEBOUNCE|PIO_PULLUP);
+	
+	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_MASK, PIO_DEFAULT);
+	
+	pio_handler_set(BUT1_PIO,ID_PIOD,BUT1_MASK,PIO_IT_FALL_EDGE, magsense_callback);
+	// Ativa interrupção no hardware
+	pio_enable_interrupt(PIOD, BUT1_MASK);
+
+	// Configura NVIC para receber interrupcoes do PIO do botao
+	// com prioridade 4 (quanto mais próximo de 0 maior)
+	NVIC_EnableIRQ(ID_PIOD);
+	NVIC_SetPriority(ID_PIOD, 3); // Prioridade 4
+}
+
+static float get_time_rtt(){
+	uint ul_previous_time = rtt_read_timer_value(RTT);
+}
+
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
+{
+	uint32_t ul_previous_time;
+
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
+	
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 5);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN);
+}
+
+
 
 
 void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
@@ -47,11 +175,21 @@ int main(void) {
 	board_init();
 	sysclk_init();	
 	configure_lcd();
-	
+	RTC_init();
+	float inst_vel = 0;
+	float total_dist = 0;
+	char buff_str[30];
+	Horario inicio;
+	Horario curr_time;
 	font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
 	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
 	font_draw_text(&arial_72, "102456", 50, 200, 2);
 	while(1) {
-		
+		if(update_alarm){
+			sprintf(buff_str,"%d",roda_voltas);
+			font_draw_text(&calibri_36, buff_str, 50, 150, 1);
+			rtt_reconfigure();
+			update_alarm = 0;
+		}
 	}
 }
